ba9678689804ce1f951598acc3845b0c05f4aa3f
Daniel Lasota <slasher.root@gmail.com>
2025-06-01 14:15:42 +0200

    feat: add global_tan_h, single_features_to_be_tan_h and add_constant.

--- praetorian_binance_backtester/core/backtester.py
+++ praetorian_binance_backtester/core/backtester.py
@@ -1,3 +1,5 @@
+from legatus_binance_deputy.core.deputy_registry import DeputyRegistry
+from legatus_binance_deputy.enums.deputy_mode import DeputyMode
 from praetorian_strategies import StrategyPool
 from praetorian_binance_backtester.core.backtest_session import BacktestSession
 from praetorian_binance_backtester.core.learn_session import LearnSession
@@ -19,6 +21,7 @@ class Backtester:
         self.strategy_pool = StrategyPool(config.strategies)
 
     def run(self) -> None:
+        deputy = DeputyRegistry.get_deputy(mode=DeputyMode.BACKTEST, name='daniel')
 
         learn_df = LearnSession.learn(
             list_of_list_of_asset_parameters=self.config.learn_list_of_merged_list_of_asset_parameters,
@@ -33,6 +36,9 @@ class Backtester:
             python_callback=self.strategy_pool.inform_strategies
         )
 
-        learn_df.to_csv(f'{MERGED_CSVS_NEST_CATALOG}/x.csv', index=False)
+        print(f'deputy.get_account_balance() {deputy.get_account_balance_usdt():.2f}')
+        print(f'deputy.get_account_balance_crypto() {deputy.get_account_balance_crypto("TRXUSDT")}')
+        for _ in deputy.get_deputy_order_list():
+            print(_)
 
-
+        # learn_df.to_csv(f'{MERGED_CSVS_NEST_CATALOG}/x.csv', index=False)
