abcd9709bdb7fb8a265719c32e9a3961bd232b3f
Daniel Lasota <slasher.root@gmail.com>
2025-06-11 10:43:23 +0200

    refactor: change oss to return np.ndarrays dict

--- praetorian_binance_backtester/core/backtest_session.py
+++ praetorian_binance_backtester/core/backtest_session.py
@@ -1,6 +1,7 @@
-from operator import attrgetter
 from pathlib import Path
 from typing import Callable
+
+import numpy as np
 from alive_progress import alive_bar
 from cpp_binance_orderbook import OrderBookSessionSimulator, OrderBookMetricsEntry
 import pandas as pd
@@ -16,7 +17,7 @@ class BacktestSession:
 
     __slots__ = [
         'callback',
-        '_backtest_entry_list'
+        '_backtest_order_book_metrics_entry_df'
     ]
 
     def __init__(
@@ -24,42 +25,43 @@ class BacktestSession:
             callback: Callable
     ):
         self.callback: Callable = callback
-        self._backtest_entry_list = []
 
-    def run(self, list_of_list_of_asset_parameters: list[list[AssetParameters]], variables: list[str]) -> None:
-        self._backtest_loop(list_of_list_of_asset_parameters, variables)
+    def run(self, list_of_list_of_asset_parameters: list[list[AssetParameters]], variables: list[str], save_df: bool = False) -> None:
+        self._backtest_loop(list_of_list_of_asset_parameters, variables, save_df)
 
     @measure_time
-    def _backtest_loop(self, list_of_list_of_asset_parameters: list[list[AssetParameters]], variables: list[str]) -> None:
+    def _backtest_loop(self, list_of_list_of_asset_parameters: list[list[AssetParameters]], variables: list[str], save_df: bool = False) -> None:
+        dfs: list[pd.DataFrame] = []
         print(Colors.CYAN)
         with alive_bar(len(list_of_list_of_asset_parameters), title='Backtest Session', spinner='dots_waves', force_tty=False) as bar:
-            for list_of_asset_parameters in list_of_list_of_asset_parameters:
-                csv_name = fu.get_base_of_merged_csv_filename(list_of_asset_parameters)
+            for asset_params in list_of_list_of_asset_parameters:
+                csv_name = fu.get_base_of_merged_csv_filename(asset_params)
                 csv_path = str(Path(MERGED_CSVS_NEST_CATALOG) / f"{csv_name}.csv")
 
                 oss = OrderBookSessionSimulator()
-                list_of_order_book_metrics_entry = oss.compute_backtest(
+                data: dict[str, np.ndarray] = oss.compute_backtest_numpy(
                     csv_path=csv_path,
                     variables=variables,
                     python_callback=self._cpp_binance_order_book_witness
                 )
-                self._backtest_entry_list.extend(list_of_order_book_metrics_entry)
+                if save_df:
+                    dfs.append(pd.DataFrame(data))
+
                 bar()
+
         print(Colors.RESET)
+
+        if save_df:
+            self._backtest_order_book_metrics_entry_df = pd.concat(dfs, ignore_index=True)
 
     def _cpp_binance_order_book_witness(self, orderbook_entry_metrics: OrderBookMetricsEntry):
         self.callback(orderbook_entry_metrics)
 
     @measure_time
-    def get_backtest_order_book_metrics_entry_df(self, variables: list[str]) -> pd.DataFrame:
-        getters = {var: attrgetter(var) for var in variables}
-        data = {var: [] for var in variables}
-
-        for entry in self._backtest_entry_list:
-            for var, getter in getters.items():
-                data[var].append(getter(entry))
-
-        return pd.DataFrame(data)
+    def get_backtest_order_book_metrics_entry_df(self) -> pd.DataFrame:
+        if self._backtest_order_book_metrics_entry_df is None:
+            raise RuntimeError("BacktestSession.run(save=True) must be called first")
+        return self._backtest_order_book_metrics_entry_df
 
     def get_final_order_book_metrics_entry(self) -> OrderBookMetricsEntry:
-        return self._backtest_entry_list[-1]
+        return self._backtest_order_book_metrics_entry_df.iloc[-1]
